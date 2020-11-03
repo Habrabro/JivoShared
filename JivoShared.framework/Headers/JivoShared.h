27cfb78657c36d494494ebd3d1b9bb6e8f9c7603
Anton_Karpushko <habrabro@gmail.com>
2020-11-03 18:15:05 +0300

    master: - added JivoShared.framework.

---
+++ JivoShared.framework/Headers/JivoShared.h
@@ -0,0 +1,18 @@
+//
+//  JivoShared.h
+//  JivoShared
+//
+//  Created by macbook on 02.11.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JivoShared.
+FOUNDATION_EXPORT double JivoSharedVersionNumber;
+
+//! Project version string for JivoShared.
+FOUNDATION_EXPORT const unsigned char JivoSharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JivoShared/PublicHeader.h>
+
+
